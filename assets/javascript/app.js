0508fd5a6bb2f07053b5906c5fd8b6f7e6d25c9e
Jack Walker <jackw2050@gmail.com>
2016-05-25 22:44:13 -0500

    daily

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,105 +1,92 @@
 $(document).ready(function() {
 
-// -------------------------------------------------
-//				Global variables
-// -------------------------------------------------
+    // -------------------------------------------------
+    //        Global variables
+    // -------------------------------------------------
 
 
-
-
-// class of array?  or aray of classes
-// question type (text, audio, video)
-// correct answer
-// pool of incorrect answers(min 3, prefer 5+)
-
-
-// randomize array order ( create new array of questionArray.length)
-
-
-//  enter loop for questions for(i = 0; i < questionArray.length; i++)  on game time out break
-// alt is while loop
-
-// create audio player
-// create video player or youtube player
-      // 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '180',
-          width: '320',
-          videoId: 'M7lc1UVf-VE',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-//startVideo();
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
+    var triviaArray = [{
+            name: "Zootopia",
+            video: " <iframe id='ytplayer ' type='text/html ' width='100% ' height='300px ' src=https://www.youtube.com/embed/uVLSe2uhLXo?autoplay=1&controls=0&enablejsapi=1&fs=0&rel=0&showinfo=0&iv_load_policy=3  ></iframe>",
+            questionLine: "This video is from the Japan release of what movie?",
+            correctAnswer: "Zootopia",
+            wrongAnswer: ["Outlander", "Animal Mania", "Madagascar"]
         }
-      }
 
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
+    ];
 
 
+    loadData();
 
 
-      function startVideo() {
-        player.startVideo();
-      }
+    function loadData() {
+        $("#player").append(triviaArray[0].video);
+        $(".question").text(triviaArray[0].questionLine);
+     //   $("#answer1").text(triviaArray[0].correctAnswer);
 
-      function stopVideo() {
-        player.stopVideo();
-      }
+// change this to append to <ol>
+        $("ol").append("<li id ='answer1'>" + triviaArray[0].correctAnswer + "</li");
+        $("ol").append("<li id ='answer2'>" + triviaArray[0].wrongAnswer[0] + "</li");
+        $("ol").append("<li id ='answer3'>" + triviaArray[0].wrongAnswer[1] + "</li");
+        $("ol").append("<li id ='answer4'>" + triviaArray[0].wrongAnswer[2] + "</li");
+     //   $("#answer2").text(triviaArray[0].wrongAnswer[0]);
+     //   $("#answer3").text(triviaArray[0].wrongAnswer[1]);
+     //   $("#answer4").text(triviaArray[0].wrongAnswer[2]);
+    }
 
 
 
 
-// test of array shuffle
-var arr = [1, 2, 3, 4];
-shuffle(arr);
-console.log(arr);
+    // class of array?  or aray of classes
+    // question type (text, audio, video)
+    // correct answer
+    // pool of incorrect answers(min 3, prefer 5+)
 
 
-// shuffle question array
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+    // randomize array order ( create new array of questionArray.length)
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+    //  enter loop for questions for(i = 0; i < questionArray.length; i++)  on game time out break
+    // alt is while loop
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+    $("li").hover(function() {
+        $(this).css("background-color", "lightblue");
+    }, function() {
+        $(this).css("background-color", "white");
+    });
 
-  return array;
-}
+
+    // check which answer is selected
+    $("input").on("click", function() {
+        $("#log").html($("input:checked").val() + " is checked!");
+    });
+
+    // test of array shuffle
+    var arr = [1, 2, 3, 4];
+    shuffle(arr);
+    console.log(arr);
+
+
+    // shuffle question array
+    function shuffle(array) {
+        var currentIndex = array.length,
+            temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
 
 
 }); // document ready
-
-
